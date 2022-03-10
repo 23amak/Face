624c7e7981a3bcdfaeb5cbd4d454429baad2b8af
Anson Mak <23amak@shcp.edu>
2022-03-10 14:05:15 -0800

    Update face.py

--- face.py
+++ face.py
@@ -27,10 +27,10 @@ def load_image( infilename ) :
     img = img.resize((64,64))
     data = np.asarray( img, dtype="int32" )
     return data
-load_image(path + "000001.png").shape
-plt.imshow(load_image(path + "000451.png"))
-plt.imshow(load_image(path + "000451.png"))
-plt.imshow(load_image(path + "000451.png"))
+load_image(path + "img_align_celeba").shape
+plt.imshow(load_image(path + "000451.jpg"))
+plt.imshow(load_image(path + "000451.jpg"))
+plt.imshow(load_image(path + "000451.jpg"))
 
 train_images = np.array(os.listdir(path))
 np.random.shuffle(train_images)
