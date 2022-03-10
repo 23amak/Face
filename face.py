7020183370d70214ebc8e9ff6aaad1d2dcc2bf93
Anson Mak <23amak@shcp.edu>
2022-03-10 15:40:59 -0800

    Update face.py

--- face.py
+++ face.py
@@ -1,291 +1,162 @@
 import tensorflow as tf
-from tensorflow.python.client import device_lib
-import glob # The glob module finds all the pathnames matching a specified pattern according to the rules used by the Unix shell, although results are returned in arbitrary order. 
-import imageio # Imageio is a Python library that provides an easy interface to read and write a wide range of image data, including animated images, volumetric data, and scientific formats. 
-
-import matplotlib.pyplot as plt 
-import tensorflow.keras.layers as layers # Keras layers API
-import time
-from IPython import display # For displaying image
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-import os
+import numpy as np
+import matplotlib.pyplot as plt
 import PIL
+from tensorflow.keras import layers
+import os
+import tarfile
+my_tar = tarfile.open('lfw.tgz')
+my_tar.extractall('./lfw') # specify which folder to extract to
+my_tar.close()
 
-"GPU" + device_lib.list_local_devices()[-1].physical_device_desc.split(",")[1]
+#%matplotlib inline
 
-tf.__version__
+def get_ds(path):
+  img = tf.io.read_file(path)
+  img = tf.image.decode_jpeg(img,channels=3)
+  img = tf.image.convert_image_dtype(img,tf.float32)
+  img = tf.divide(tf.subtract(tf.multiply(img,255),127.5),127.5)
+  return tf.image.resize(img,(64,64)) 
 
-path = "C:/Users/foggy/Documents/img_align_celeba"
+images = []
+for i in os.scandir('lfw/lfw'):
+  for j in os.scandir(i.path):
+    images.append(j.path)
 
-def load_image( infilename ) :
-    img = PIL.Image.open( infilename )
-    img = img.crop([25,65,153,193])
-    img = img.resize((64,64))
-    data = np.asarray( img, dtype="int32" )
-    return data
-load_image(path).shape
-plt.imshow(load_image(path + "000451.jpg"))
-plt.imshow(load_image(path + "000451.jpg"))
-plt.imshow(load_image(path + "000451.jpg"))
-
-train_images = np.array(os.listdir(path))
-np.random.shuffle(train_images)
-BUFFER_SIZE = 100#200000 # number of images in training i think
-BATCH_SIZE = 5#500 # This is just the standard number for batch size. Google for more info
-# shuffle and batch the data
-np.random.shuffle(train_images)
-train_images = np.split(train_images[:BUFFER_SIZE],BATCH_SIZE) 
-
-np.array(train_images).shape
-
+images = tf.data.Dataset.from_tensor_slices(images)
+BATCH_SIZE = 64
+train_images = images.map(get_ds,num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(BATCH_SIZE).shuffle(60000)
+ 
 def make_generator_model():
-  # In Keras, you assemble layers to build models. A model is (usually) a graph of layers. The most common type of model is a stack of layers
-  model = tf.keras.Sequential()
-  
-  """
-  Add a densely-connected layer to the model
-  the model will take as input arrays of shape (*, 100).
-  And and output arrays of shape (*, 7*7*256)
-  
-  after the first layer, you don't need to specify the size of the input anymore
-  Afterwards, we do automatic shape inference
-  """
-  model.add(layers.Dense(4*4*1024, use_bias = False, input_shape = (100,)))
-  
-  """
-  You can think about batch normalization as doing preprocessing at every layer of the network.
-  Accelerating Deep Network Training by Reducing Internal Covariate Shift
-  """
-  model.add(layers.BatchNormalization())
-  
-  """
-  ReLU is linear (identity) for all positive values, and zero for all negative values. 
-  Leaky ReLU has a small slope for negative values, instead of altogether zero. For example, leaky ReLU may have y = 0.01x when x < 0
-  """
-  model.add(layers.LeakyReLU())
-  
-  # reshape the output from something flattened to something with a shape of (7,7,256)
-  model.add(layers.Reshape(( 4, 4, 1024)))
-  assert model.output_shape == (None, 4, 4, 1024) # Note: None is the batch size
-  
-  """
-  The generator uses a transposed convolutional layer (Upsampling) layers to produce an image from seed (random noise).
-  
-  128 is the dimensionality of the output space
-  (5,5) specifies the height and width of the 2D convolution window
-  strides = (1,1) specifies the strides of the convolution along the height and width 
-  
-  """
-  model.add(layers.Conv2DTranspose(512, (5, 5), strides = (2,2), padding = "same", use_bias = False))
-  assert model.output_shape == (None, 8, 8, 512)
-  model.add(layers.BatchNormalization())
-  model.add(layers.LeakyReLU())
-  
-  # Another transposed convolutional layer (upsampling)
-  model.add(layers.Conv2DTranspose(256, (5,5), strides = (2,2), padding = "same", use_bias = False))
-  assert model.output_shape == (None, 16, 16, 256)
-  model.add(layers.BatchNormalization())
-  model.add(layers.LeakyReLU())
-  
-  # Another transposed convolutional layer (upsampling)
-  model.add(layers.Conv2DTranspose(128, (5,5), strides = (2,2), padding = "same", use_bias = False))
-  assert model.output_shape == (None, 32, 32, 128)
-  model.add(layers.BatchNormalization())
-  model.add(layers.LeakyReLU())
-  
-  # Final output layer also a convolutional layer (upsampling), sigmoid goes from 0 to 1
-  model.add(layers.Conv2DTranspose(3, (5,5), strides = (2,2), padding = "same", use_bias = False, activation = "sigmoid"))
-  assert model.output_shape == (None, 64, 64, 3)
-  
-  return model
+    model = tf.keras.models.Sequential()
+    model.add(layers.Dense(8*8*128,input_shape=(100,),use_bias=False))    # creates output shape of 7,7 with number of neurons - [7,7,256]
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    
+    model.add(layers.Reshape((8,8,128)))
+    assert model.output_shape == (None, 8, 8, 128)
+    
+    model.add(layers.Conv2DTranspose(64,(5,5),strides=(1,1),padding='same',use_bias=False))    # stride (1,1) keeps the same shape as that of input
+    assert model.output_shape == (None, 8, 8, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    ## output of shape (7,7,128)
+    
+    model.add(layers.Conv2DTranspose(64,(5,5),strides=(2,2),padding='same',use_bias=False))     # stride (2,2) doubles the size of the input 
+    assert model.output_shape == (None, 16, 16, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    ## output shape (14,14,64)
+
+    model.add(layers.Conv2DTranspose(64,(5,5),strides=(2,2),padding='same',use_bias=False))     # stride (2,2) doubles the size of the input 
+    assert model.output_shape == (None, 32, 32, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    
+    model.add(layers.Conv2DTranspose(3,(5,5),strides=(2,2),padding='same',activation='tanh',use_bias=False))
+    assert model.output_shape == (None, 64, 64, 3)
+    ## output shape (28,28,1) the required shape
+    
+    return model
+generator = make_generator_model()
+noise = tf.random.normal([1,100])
+generated_image = generator(noise,training=False)
+# x = generated_image.numpy().reshape(64,64,1)
+plt.imshow(generated_image[0]*127.5+127.5)
 
 def make_discriminator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', 
-                                     input_shape=[64, 64, 3]))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3)) # Dropout consists in randomly setting a fraction rate of input units to 0 at each update during training time, which helps prevent overfitting.
-      
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
-    
-    """
-    Flattens the input. Does not affect the batch size.
-    If inputs are shaped (batch,) without a channel dimension, then flattening adds an extra channel dimension and output shapes are (batch, 1).
-    """
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1))
-     
-    return model
+  model = tf.keras.models.Sequential()
+  model.add(layers.Conv2D(64,(5,5),strides=(2,2),padding='same',input_shape=[64,64,3]))
+  model.add(layers.LeakyReLU())
+  model.add(layers.Dropout(0.3))
 
+  model.add(layers.Conv2D(128,(5,5),strides=(2,2),padding='same'))
+  model.add(layers.LeakyReLU())
+  model.add(layers.Dropout(0.3))
+
+  model.add(layers.Flatten())
+  model.add(layers.Dense(1))
+
+  return model
 discriminator = make_discriminator_model()
 decision = discriminator(generated_image)
 print(decision)
 
-# This method returns a helper function to compute cross entropy loss
-cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits = True)
-
-"""
-Discriminator Loss
-
-This method quantifies how well the discriminator is able to distinguish real images from fakes. It compares the discriminator's predicitions on real images to an array of 1s
-and the dicriminator's predicitons on fake (generated) images to an array of 0s.
-"""
-@tf.function
-def discriminator_loss(real_output, fake_output):
-  real_loss = cross_entropy(tf.ones_like(real_output), real_output)
-  fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+def discriminator_loss(real_output,fake_output):
+  real_loss = cross_entropy(tf.ones_like(real_output),real_output)
+  fake_loss = cross_entropy(tf.zeros_like(fake_output),fake_output)
   total_loss = real_loss + fake_loss
-  
   return total_loss
-
-"""
-Generator Loss
-
-The generator's loss quantifies how well it was able to trick the discrimator. Intuitively, if the generator is performing well, the discriminator will classify the fake images as real (or 1).
-Here, we will compare the discriminators decisions on the generated images to an array of 1s.
-"""
-@tf.function
 def generator_loss(fake_output):
-  return cross_entropy(tf.ones_like(fake_output), fake_output)
-
-"""
-The discriminator and the generator optimizers are different since we will train two networks separately.
-The Adam optimization algorithm is an extension to stochastic gradient descent.
-Stochastic gradient descent maintains a single learning rate (termed alpha) for all weight updates and the learning rate does not change during training.
-A learning rate is maintained for each network weight (parameter) and separately adapted as learning unfolds.
-
-"""
+  return cross_entropy(tf.ones_like(fake_output),fake_output)
 generator_optimizer = tf.keras.optimizers.Adam(1e-4)
 discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
-
-# We will reuse this seed overtime (so it's easier) to visualize progress in the animated GIF
-tf.random.set_seed(1234)
-noise_dim = 100
-num_examples_to_generate = 16
-seed = tf.random.normal([num_examples_to_generate, noise_dim], seed=1)
-
-seed
-
-EPOCHS = 100
-
-"""
-The training loop begins with generator receiving a random seed as input. 
-That seed is used to produce an image. The discriminator is then used to classify real images (drawn from the training set) and fakes images (produced by the generator). 
-The loss is calculated for each of these models, and the gradients are used to update the generator and discriminator.
-"""
-
-# Notice the use of tf.function
-# This annotation causes the function to be "compiled"
+import os
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir,'ckpt')
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+EPOCHS = 200
+noise_dims = 100
+num_egs_to_generate = 16
+seed = tf.random.normal([num_egs_to_generate,noise_dims])
+@tf.function
 def train_step(images):
-    noise = tf.random.normal([BATCH_SIZE, noise_dim])
-    
-    # take the image links and return a cropped image
-    new_images = []
-    for file_name in images:
-        new_pic = load_image( path + file_name)
-        new_images.append(new_pic)
-    
-    images = np.array(new_images)
-    images = images.reshape(images.shape[0], 64, 64, 3).astype('float32') # puts each number in its own numpy array so instead of [1,2,3] gonna be [[1], [2], [3]]
-    images = (images) / 255 # normalize to [0,1]
-    
+  noise = tf.random.normal([BATCH_SIZE,noise_dims])
 
+  with tf.GradientTape() as gen_tape, tf.GradientTape() as dis_tape:
+    generated_images = generator(noise,training=True)
 
-    """
-GradientTape() Records operations for automatic differentiation. Operations are recorded if 
-they are executed within this context manager and at least one of their inputs is being "watched".
-    """
-    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-      generated_images = generator(noise, training=True)
+    real_output = discriminator(images,training=True)
+    fake_output = discriminator(generated_images,training=True)
 
-      real_output = discriminator(images, training=True)
-      fake_output = discriminator(generated_images, training=True)
+    gen_loss = generator_loss(fake_output)
+    disc_loss = discriminator_loss(real_output,fake_output)
 
-      gen_loss = generator_loss(fake_output)
-      disc_loss = discriminator_loss(real_output, fake_output)
-    
-    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
-    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+  gen_gradients = gen_tape.gradient(gen_loss,generator.trainable_variables)
+  dis_gradients = dis_tape.gradient(disc_loss,discriminator.trainable_variables)
 
-    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables)) # The zip() function returns an iterator of tuples based on the iterable object.
-    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
-    
-    images = None
-
-    #@tf.function
-def train(dataset, epochs):  
-  tf.print("Starting man!")
+  generator_optimizer.apply_gradients(zip(gen_gradients,generator.trainable_variables))
+  discriminator_optimizer.apply_gradients(zip(dis_gradients,discriminator.trainable_variables))
+from IPython import display
+import time
+def train(dataset,epochs):
   for epoch in range(epochs):
     start = time.time()
-    tf.print("Starting Epoch:", epoch)
-    batch_count = 1
-    for image_batch in dataset:
-      #tf.print("Batch:", batch_count)
-      train_step(image_batch)
-      #print("Batch:", batch_count, "Complete")
-      batch_count += 1
-    
-
-    # Produce images for the GIF as we go
+    for batch in dataset:
+      train_step(batch)
     display.clear_output(wait=True)
-    generate_and_save_images(generator,
-                             epoch + 1,
-                             seed)
-    
-    tf.print("Epoch:", epoch, "finished")
-    tf.print()
-    
-    # Save the model every epochs
-    checkpoint.save(file_prefix = checkpoint_prefix)
-    
-    tf.print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
-    
-  # Generate after the final epoch
-  display.clear_output(wait=True)
-  generate_and_save_images(generator,
-                           epochs,
-                           seed)
-def generate_and_save_images(model, epoch, test_input):
-  # Notice `training` is set to False. 
-  # This is so all layers run in inference mode (batchnorm).
-  predictions = model(test_input, training=False).numpy()
+    generate_and_save_output(generator,epoch+1,seed)
 
-  fig = plt.figure(figsize=(4,4))
+    if (epoch+1)%15 == 0:
+      checkpoint.save(file_prefix=checkpoint_prefix)
+
+    print(f'Time for epoch {epoch + 1} is {time.time()-start}')
   
+  display.clear_output(wait=True)
+  generate_and_save_output(generator,epochs,seed)
+def generate_and_save_output(model,epoch,test_input):
+
+  predictions = model(test_input,training=False)
+  # predictions = predictions.numpy().reshape(16,64,64,1)
+  fig = plt.figure(figsize=(4,4))
+  # print(predictions)
   for i in range(predictions.shape[0]):
-      plt.subplot(4, 4, i+1)
-      plt.imshow(predictions[i])
-      plt.axis('off')
-        
-  plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
-  plt.show()
-from IPython.display import Image
-#Image(filename='image_at_epoch_0060.png') 
-display.Image(filename="dcgan_celebA.gif.png")
-
-train(train_images, EPOCHS)
-
-fig = plt.figure(figsize=(4,4))
-predictions = checkpoint.generator(seed, training = False).numpy()
-for i in range(seed.shape[0]):
     plt.subplot(4,4,i+1)
-    plt.imshow(predictions[i], interpolation="nearest")
+    plt.imshow((predictions[i]*127.5+127.5).numpy().astype(np.uint8),cmap='gray')
     plt.axis('off')
-    
-plt.show()
-noise = tf.random.normal([1,100]) # shape is 1, 100
-random_face = checkpoint.generator(noise, training = False).numpy()[0]
-plt.imshow(random_face, interpolation='nearest')
-plt.axis("off")
-plt.show()
+  plt.savefig(f'image_at_epoch_{epoch}.png')
+  plt.show()
+train(train_images,EPOCHS)
 
-with imageio.get_writer('dcgan.gif', mode='I') as writer:
+import imageio
+import glob
+anim_file = 'dcgan.gif'
+
+with imageio.get_writer(anim_file, mode='I') as writer:
   filenames = glob.glob('image*.png')
   filenames = sorted(filenames)
   last = -1
@@ -299,9 +170,10 @@ with imageio.get_writer('dcgan.gif', mode='I') as writer:
     writer.append_data(image)
   image = imageio.imread(filename)
   writer.append_data(image)
-    
-# A hack to display the GIF inside this notebook
-os.rename('dcgan.gif', 'dcgan_celebA.gif.png')
 
-display.Image(filename="dcgan_celebA.gif.png")
+import IPython
+if IPython.version_info > (6,2,0,''):
+  display.Image(filename=anim_file)
+new_image = generator(tf.random.normal([1,100]),training=False)
+plt.imshow(new_image[0,:,:,:])
 
