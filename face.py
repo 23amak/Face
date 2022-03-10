a2ed5e52d97229616c92b94f8a89661a85875236
Anson Mak <23amak@shcp.edu>
2022-03-10 15:44:49 -0800

    Update face.py

--- face.py
+++ face.py
@@ -1,180 +1,217 @@
+"""
+Title: DCGAN to generate face images
+Author: [fchollet](https://twitter.com/fchollet)
+Date created: 2019/04/29
+Last modified: 2021/01/01
+Description: A simple DCGAN trained using `fit()` by overriding `train_step` on CelebA images.
+"""
+"""
+## Setup
+"""
+
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 import numpy as np
 import matplotlib.pyplot as plt
-import PIL
-from tensorflow.keras import layers
 import os
+import gdown
+from zipfile import ZipFile
+
 """
-import tarfile
-my_tar = tarfile.open('lfw.tgz')
-my_tar.extractall('./lfw') # specify which folder to extract to
-my_tar.close()
+## Prepare CelebA data
+We'll use face images from the CelebA dataset, resized to 64x64.
 """
-#%matplotlib inline
 
-def get_ds(path):
-  img = tf.io.read_file(path)
-  img = tf.image.decode_jpeg(img,channels=3)
-  img = tf.image.convert_image_dtype(img,tf.float32)
-  img = tf.divide(tf.subtract(tf.multiply(img,255),127.5),127.5)
-  return tf.image.resize(img,(64,64)) 
+os.makedirs("celeba_gan")
 
-images = []
-for i in os.scandir('lfw/lfw'):
-  for j in os.scandir(i.path):
-    images.append(j.path)
+url = "https://drive.google.com/uc?id=1O7m1010EJjLE5QxLZiM9Fpjs7Oj6e684"
+output = "celeba_gan/data.zip"
+gdown.download(url, output, quiet=True)
 
-images = tf.data.Dataset.from_tensor_slices(images)
-BATCH_SIZE = 64
-train_images = images.map(get_ds,num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(BATCH_SIZE).shuffle(60000)
- 
-def make_generator_model():
-    model = tf.keras.models.Sequential()
-    model.add(layers.Dense(8*8*128,input_shape=(100,),use_bias=False))    # creates output shape of 7,7 with number of neurons - [7,7,256]
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-    
-    model.add(layers.Reshape((8,8,128)))
-    assert model.output_shape == (None, 8, 8, 128)
-    
-    model.add(layers.Conv2DTranspose(64,(5,5),strides=(1,1),padding='same',use_bias=False))    # stride (1,1) keeps the same shape as that of input
-    assert model.output_shape == (None, 8, 8, 64)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-    ## output of shape (7,7,128)
-    
-    model.add(layers.Conv2DTranspose(64,(5,5),strides=(2,2),padding='same',use_bias=False))     # stride (2,2) doubles the size of the input 
-    assert model.output_shape == (None, 16, 16, 64)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-    ## output shape (14,14,64)
+with ZipFile("celeba_gan/data.zip", "r") as zipobj:
+    zipobj.extractall("celeba_gan")
 
-    model.add(layers.Conv2DTranspose(64,(5,5),strides=(2,2),padding='same',use_bias=False))     # stride (2,2) doubles the size of the input 
-    assert model.output_shape == (None, 32, 32, 64)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-    
-    model.add(layers.Conv2DTranspose(3,(5,5),strides=(2,2),padding='same',activation='tanh',use_bias=False))
-    assert model.output_shape == (None, 64, 64, 3)
-    ## output shape (28,28,1) the required shape
-    
-    return model
-generator = make_generator_model()
-noise = tf.random.normal([1,100])
-generated_image = generator(noise,training=False)
-# x = generated_image.numpy().reshape(64,64,1)
-plt.imshow(generated_image[0]*127.5+127.5)
+"""
+Create a dataset from our folder, and rescale the images to the [0-1] range:
+"""
 
-def make_discriminator_model():
-  model = tf.keras.models.Sequential()
-  model.add(layers.Conv2D(64,(5,5),strides=(2,2),padding='same',input_shape=[64,64,3]))
-  model.add(layers.LeakyReLU())
-  model.add(layers.Dropout(0.3))
+dataset = keras.preprocessing.image_dataset_from_directory(
+    "celeba_gan", label_mode=None, image_size=(64, 64), batch_size=32
+)
+dataset = dataset.map(lambda x: x / 255.0)
 
-  model.add(layers.Conv2D(128,(5,5),strides=(2,2),padding='same'))
-  model.add(layers.LeakyReLU())
-  model.add(layers.Dropout(0.3))
 
-  model.add(layers.Flatten())
-  model.add(layers.Dense(1))
+"""
+Let's display a sample image:
+"""
 
-  return model
-discriminator = make_discriminator_model()
-decision = discriminator(generated_image)
-print(decision)
 
-cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-def discriminator_loss(real_output,fake_output):
-  real_loss = cross_entropy(tf.ones_like(real_output),real_output)
-  fake_loss = cross_entropy(tf.zeros_like(fake_output),fake_output)
-  total_loss = real_loss + fake_loss
-  return total_loss
-def generator_loss(fake_output):
-  return cross_entropy(tf.ones_like(fake_output),fake_output)
-generator_optimizer = tf.keras.optimizers.Adam(1e-4)
-discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
-import os
-checkpoint_dir = './training_checkpoints'
-checkpoint_prefix = os.path.join(checkpoint_dir,'ckpt')
-checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-                                 discriminator_optimizer=discriminator_optimizer,
-                                 generator=generator,
-                                 discriminator=discriminator)
-EPOCHS = 200
-noise_dims = 100
-num_egs_to_generate = 16
-seed = tf.random.normal([num_egs_to_generate,noise_dims])
-@tf.function
-def train_step(images):
-  noise = tf.random.normal([BATCH_SIZE,noise_dims])
+for x in dataset:
+    plt.axis("off")
+    plt.imshow((x.numpy() * 255).astype("int32")[0])
+    break
 
-  with tf.GradientTape() as gen_tape, tf.GradientTape() as dis_tape:
-    generated_images = generator(noise,training=True)
 
-    real_output = discriminator(images,training=True)
-    fake_output = discriminator(generated_images,training=True)
+"""
+## Create the discriminator
+It maps a 64x64 image to a binary classification score.
+"""
 
-    gen_loss = generator_loss(fake_output)
-    disc_loss = discriminator_loss(real_output,fake_output)
+discriminator = keras.Sequential(
+    [
+        keras.Input(shape=(64, 64, 3)),
+        layers.Conv2D(64, kernel_size=4, strides=2, padding="same"),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2D(128, kernel_size=4, strides=2, padding="same"),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2D(128, kernel_size=4, strides=2, padding="same"),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Flatten(),
+        layers.Dropout(0.2),
+        layers.Dense(1, activation="sigmoid"),
+    ],
+    name="discriminator",
+)
+discriminator.summary()
 
-  gen_gradients = gen_tape.gradient(gen_loss,generator.trainable_variables)
-  dis_gradients = dis_tape.gradient(disc_loss,discriminator.trainable_variables)
+"""
+## Create the generator
+It mirrors the discriminator, replacing `Conv2D` layers with `Conv2DTranspose` layers.
+"""
 
-  generator_optimizer.apply_gradients(zip(gen_gradients,generator.trainable_variables))
-  discriminator_optimizer.apply_gradients(zip(dis_gradients,discriminator.trainable_variables))
-from IPython import display
-import time
-def train(dataset,epochs):
-  for epoch in range(epochs):
-    start = time.time()
-    for batch in dataset:
-      train_step(batch)
-    display.clear_output(wait=True)
-    generate_and_save_output(generator,epoch+1,seed)
+latent_dim = 128
 
-    if (epoch+1)%15 == 0:
-      checkpoint.save(file_prefix=checkpoint_prefix)
+generator = keras.Sequential(
+    [
+        keras.Input(shape=(latent_dim,)),
+        layers.Dense(8 * 8 * 128),
+        layers.Reshape((8, 8, 128)),
+        layers.Conv2DTranspose(128, kernel_size=4, strides=2, padding="same"),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2DTranspose(256, kernel_size=4, strides=2, padding="same"),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2DTranspose(512, kernel_size=4, strides=2, padding="same"),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2D(3, kernel_size=5, padding="same", activation="sigmoid"),
+    ],
+    name="generator",
+)
+generator.summary()
 
-    print(f'Time for epoch {epoch + 1} is {time.time()-start}')
-  
-  display.clear_output(wait=True)
-  generate_and_save_output(generator,epochs,seed)
-def generate_and_save_output(model,epoch,test_input):
+"""
+## Override `train_step`
+"""
 
-  predictions = model(test_input,training=False)
-  # predictions = predictions.numpy().reshape(16,64,64,1)
-  fig = plt.figure(figsize=(4,4))
-  # print(predictions)
-  for i in range(predictions.shape[0]):
-    plt.subplot(4,4,i+1)
-    plt.imshow((predictions[i]*127.5+127.5).numpy().astype(np.uint8),cmap='gray')
-    plt.axis('off')
-  plt.savefig(f'image_at_epoch_{epoch}.png')
-  plt.show()
-train(train_images,EPOCHS)
 
-import imageio
-import glob
-anim_file = 'dcgan.gif'
+class GAN(keras.Model):
+    def __init__(self, discriminator, generator, latent_dim):
+        super(GAN, self).__init__()
+        self.discriminator = discriminator
+        self.generator = generator
+        self.latent_dim = latent_dim
 
-with imageio.get_writer(anim_file, mode='I') as writer:
-  filenames = glob.glob('image*.png')
-  filenames = sorted(filenames)
-  last = -1
-  for i,filename in enumerate(filenames):
-    frame = 2*(i**0.5)
-    if round(frame) > round(last):
-      last = frame
-    else:
-      continue
-    image = imageio.imread(filename)
-    writer.append_data(image)
-  image = imageio.imread(filename)
-  writer.append_data(image)
+    def compile(self, d_optimizer, g_optimizer, loss_fn):
+        super(GAN, self).compile()
+        self.d_optimizer = d_optimizer
+        self.g_optimizer = g_optimizer
+        self.loss_fn = loss_fn
+        self.d_loss_metric = keras.metrics.Mean(name="d_loss")
+        self.g_loss_metric = keras.metrics.Mean(name="g_loss")
 
-import IPython
-if IPython.version_info > (6,2,0,''):
-  display.Image(filename=anim_file)
-new_image = generator(tf.random.normal([1,100]),training=False)
-plt.imshow(new_image[0,:,:,:])
+    @property
+    def metrics(self):
+        return [self.d_loss_metric, self.g_loss_metric]
 
+    def train_step(self, real_images):
+        # Sample random points in the latent space
+        batch_size = tf.shape(real_images)[0]
+        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
+
+        # Decode them to fake images
+        generated_images = self.generator(random_latent_vectors)
+
+        # Combine them with real images
+        combined_images = tf.concat([generated_images, real_images], axis=0)
+
+        # Assemble labels discriminating real from fake images
+        labels = tf.concat(
+            [tf.ones((batch_size, 1)), tf.zeros((batch_size, 1))], axis=0
+        )
+        # Add random noise to the labels - important trick!
+        labels += 0.05 * tf.random.uniform(tf.shape(labels))
+
+        # Train the discriminator
+        with tf.GradientTape() as tape:
+            predictions = self.discriminator(combined_images)
+            d_loss = self.loss_fn(labels, predictions)
+        grads = tape.gradient(d_loss, self.discriminator.trainable_weights)
+        self.d_optimizer.apply_gradients(
+            zip(grads, self.discriminator.trainable_weights)
+        )
+
+        # Sample random points in the latent space
+        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
+
+        # Assemble labels that say "all real images"
+        misleading_labels = tf.zeros((batch_size, 1))
+
+        # Train the generator (note that we should *not* update the weights
+        # of the discriminator)!
+        with tf.GradientTape() as tape:
+            predictions = self.discriminator(self.generator(random_latent_vectors))
+            g_loss = self.loss_fn(misleading_labels, predictions)
+        grads = tape.gradient(g_loss, self.generator.trainable_weights)
+        self.g_optimizer.apply_gradients(zip(grads, self.generator.trainable_weights))
+
+        # Update metrics
+        self.d_loss_metric.update_state(d_loss)
+        self.g_loss_metric.update_state(g_loss)
+        return {
+            "d_loss": self.d_loss_metric.result(),
+            "g_loss": self.g_loss_metric.result(),
+        }
+
+
+"""
+## Create a callback that periodically saves generated images
+"""
+
+
+class GANMonitor(keras.callbacks.Callback):
+    def __init__(self, num_img=3, latent_dim=128):
+        self.num_img = num_img
+        self.latent_dim = latent_dim
+
+    def on_epoch_end(self, epoch, logs=None):
+        random_latent_vectors = tf.random.normal(shape=(self.num_img, self.latent_dim))
+        generated_images = self.model.generator(random_latent_vectors)
+        generated_images *= 255
+        generated_images.numpy()
+        for i in range(self.num_img):
+            img = keras.preprocessing.image.array_to_img(generated_images[i])
+            img.save("generated_img_%03d_%d.png" % (epoch, i))
+
+
+"""
+## Train the end-to-end model
+"""
+
+epochs = 1  # In practice, use ~100 epochs
+
+gan = GAN(discriminator=discriminator, generator=generator, latent_dim=latent_dim)
+gan.compile(
+    d_optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+    g_optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+    loss_fn=keras.losses.BinaryCrossentropy(),
+)
+
+gan.fit(
+    dataset, epochs=epochs, callbacks=[GANMonitor(num_img=10, latent_dim=latent_dim)]
+)
+
+"""
+Some of the last generated images around epoch 30
+(results keep improving after that):
+![results](https://i.imgur.com/h5MtQZ7l.png)
+"""
