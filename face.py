f8b9d3446baadae7fbe2686999c7e72d35c2682d
Anson Mak <23amak@shcp.edu>
2022-03-22 16:01:33 -0700

    Update face.py

--- face.py
+++ face.py
@@ -110,7 +110,7 @@ def make_generator_model():
 
 generator = make_generator_model()
 
-noise = tf.random.normal([1,256]) # shape is 1, 100
+noise = tf.random.normal([1,100]) # shape is 1, 100
 generated_image = generator(noise, training = False)
 plt.imshow(generated_image[0], interpolation="nearest")
 
@@ -197,7 +197,7 @@ modelcheckpoint_last = ModelCheckpoint(checkpoint_path_last,
 
 # We will reuse this seed overtime (so it's easier) to visualize progress in the animated GIF
 tf.random.set_seed(1234)
-noise_dim = 256
+noise_dim = 100
 num_examples_to_generate = 16
 seed = tf.random.normal([num_examples_to_generate, noise_dim], seed=1)
 
@@ -311,7 +311,7 @@ for i in range(seed.shape[0]):
     plt.axis('off')
     
 plt.show()
-noise = tf.random.normal([1,256]) # shape is 1, 100
+noise = tf.random.normal([1,100]) # shape is 1, 100
 random_face = checkpoint.generator(noise, training = False).numpy()[0]
 plt.imshow(random_face, interpolation='nearest')
 plt.axis("off")
