6905eaed7e97bade29d7793fba6e2182b6a95b87
Anson Mak <23amak@shcp.edu>
2022-03-29 15:19:25 -0700

    Update face.py

--- face.py
+++ face.py
@@ -39,10 +39,12 @@ plt.imshow(load_image(path + "000451.jpg"))
 train_images = np.array(os.listdir(path))
 np.random.shuffle(train_images)
 BUFFER_SIZE = 50 #200000 #100 #number of images in training i think
-BATCH_SIZE = 5 #500 # This is just the standard number for batch size. Google for more info
+BATCH_SIZE = 32 #500 # This is just the standard number for batch size. Google for more info
 # shuffle and batch the data
-np.random.shuffle(train_images)
-train_images = np.split(train_images[:BUFFER_SIZE],BATCH_SIZE) 
+# np.random.shuffle(train_images)
+#train_images = np.split(train_images[:BUFFER_SIZE],BATCH_SIZE) 
+train_images = np.split(train_images, BATCH_SIZE) 
+
 
 np.array(train_images).shape
 
